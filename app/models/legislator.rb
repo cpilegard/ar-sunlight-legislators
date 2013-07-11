2d442b1e728b9d7733617b4b0cbc68727e2f31d8
Cole Pilegard <c.pilegard@gmail.com>
2013-07-11 11:57:04 -0700

    update model and migration, don't need all that data

--- app/models/legislator.rb
+++ app/models/legislator.rb
@@ -1,11 +1,6 @@
 require_relative '../../db/congfig'
 
 class Legislator < ActiveRecord::Base
-	validates :title, :firstname, :lastname, :party, :state
-
-
-	def name
-		"#{self.firstname} #{self.middlename} #{self.lastname} #{self.name_suffix}"
-	end
+	validates :title, :name, :lastname, :party, :state
 
 end
